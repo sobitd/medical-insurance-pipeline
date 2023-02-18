f51ebd453f0f8c7582c00f41845e4fbb866eb5e6
sobitd <sobitdaniel@gmail.com>
2023-02-19 01:12:40 +0300

    delete undesired columns

--- etlpipeline.sh
+++ etlpipeline.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-# Read 50 rows of Data in the insurance.csv file
-INPUT=insurance.csv
-OLDIFS=$IFS
-IFS=','
-[! -f $INPUT ] && { echo "$INPUT file not found"; exit 49; }
-while read age sex bmi children smoker region charges
-do
-    echo "Age : $age"
-    echo "Sex : $sex"
-    echo "BMI : $bmi"
-    echo "Children : $children"
-    echo "Smoker : $smoker"
-    echo "Region : $region"
-    echo "Charges : $charges"
-done < $INPUT
-IFS=$OLDIFS
- 
+#Extraction
+# Retrieve the columns of insurance.csv
+csvcut -n insurance.csv
+
+#print date frome desired columns
+csvcut -c age,sex,region,charges insurance.csv | csvlook
+
+#Transformation
+#We only want age sex region charges
+cut -d, -f3,4,5 --complement insurance.csv > cleaned.csv
+
