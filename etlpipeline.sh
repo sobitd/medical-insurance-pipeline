cef18736ee063c853b4288a6d8bb3cc54ea4ad44
sobitd <sobitdaniel@gmail.com>
2023-02-19 14:12:34 +0300

    ETL Pipeline done

--- etlpipeline.sh
+++ etlpipeline.sh
@@ -2,43 +2,55 @@
 
 #Extract
 echo "Extracting Data"
+
+echo "columns in insurance.csv"
 # Retrieve the columns of insurance.csv
 csvcut -n insurance.csv
 
+
+echo " desired data inside insurance.csv"
 #print date frome desired columns
 csvcut -c age,sex,region,charges insurance.csv | csvlook
 
 #Transform
+
 echo "Transforming Data"
 #We only want age sex region charges
 cut -d, -f3,4,5 --complement insurance.csv > cleaned.csv
 
-#view cleaned.csv
-csvcut -c cleaned.csv | csvlook 
+# print cleaned.csv
+echo "cleaned data"
+csvlook cleaned.csv
 
 #pre-Load phase
+
 echo "Loading Data"
 
-#connecting to  Postgres CLI
-psql --username=postgres --host=localhost
+#Create a database called insuranced
+psql -U postgres -c "CREATE DATABASE insurancedb;"
 
-#Create database insurance
-CREATE DATABASE insurance;
 
-#connecting to database insurance
-\c insurance
+# Creating table in insurance where the csv data will be loaded
+psql -U postgres -d insurancedb -c " CREATE TABLE insured( id SERIAL PRIMARY KEY,
+		      age VARCHAR(255),
+		      sex VARCHAR(255),
+		      region VARCHAR(255),
+		      charges VARCHAR(255)
+);"
 
-#Creating table
-CREATE TABLE insured( id SERIAL,PRIMARY KEY (id), age VARCHAR(255), sex VARCHAR(255),region VARCHAR(255),charges VARCHAR(255));
 
-#To  check if table insured was created
-\dt
 
 # Load Phase
 echo " Loading data"
 
 #Send the instructions to connect to  'insurance database' and
-#copy the csv to the table 'Insured' through command pipeline.
+#copy the csv to the table 'insured' through command pipeline.
 #\c - to connect to your database
-#\COPY csv into table
-COPY insured(age,sex,region,charges) FROM cleaned.csv DELIMITER ',' CSV HEADER;
+#\COPY csv into table insured
+psql -U postgres -d insurancedb -c "\copy insured(age,sex,region,charges)
+FROM 'cleaned.csv'
+DELIMITER ','
+CSV HEADER;"
+
+
+
